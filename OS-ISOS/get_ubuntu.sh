708382cf4d101feffe3c55f89956cbecebcb39cd
Paolo Schiro <paolo@schiro.it>
2021-02-18 18:40:34 +0100

    added ubuntu focal

--- OS-ISOS/get_ubuntu.sh
+++ OS-ISOS/get_ubuntu.sh
@@ -1,2 +1,3 @@
 #curl http://ftp.nl.debian.org/debian/dists/buster/main/installer-amd64/current/images/netboot/mini.iso -o debian-buster-amd64-mini.iso
-curl http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/mini.iso -o Ubuntu-bionic-amd64-mini.iso
+#curl http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/mini.iso -o Ubuntu-bionic-amd64-mini.iso
+curl http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/current/legacy-images/netboot/mini.iso -o Ubuntu-focal-amd64-mini.iso
